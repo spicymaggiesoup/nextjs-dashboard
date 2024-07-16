e97e4895eac6bc3aed1996775b0a027b421e2811
cowkite lee <35284787+spicymaggiesoup@users.noreply.github.com>
2024-07-17 00:01:42 +0900

    Update page.tsx
    download url changed

--- app/page.tsx
+++ app/page.tsx
@@ -9,35 +9,18 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
-        <AcmeLogo/>
+        {/* <AcmeLogo/> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* 챕터-2 css @/app/ui/home.module.css 의 클래스로 즉각적용 */}
           <div className={styles.shape}></div>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>nUBoard 매뉴얼</strong> 을 다운로드 받아보세요.{' '}
+            <a href="https://github.com/spicymaggiesoup/board/blob/main/nUBoard-Guide-Admin_20230926v.pdf?raw=true" className="text-blue-500">
+              관리자페이지 매뉴얼 다운받기
             </a>
-            , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop and mobile versions"
-          />
         </div>
       </div>
     </main>
